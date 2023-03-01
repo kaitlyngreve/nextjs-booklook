7f5e1c7ba233f983566a030491bd8184da53d9ac
Kaitlyn Greve <kgreve14@gmail.com>
2023-02-28 16:06:13 -0800

    displayed images of books on frontend

--- src/pages/books/book.js
+++ src/pages/books/book.js
@@ -1,14 +1,14 @@
 
 
 export default function Book({ book }) {
-    const bookV = book.volumeInfo
-    const bookImg = book.volumeInfo.imageLinks
+    let bookV = book.volumeInfo
+    let bookImg = bookV.imageLinks
 
     return (
         <div>
             <h4>Title: {bookV.title}</h4>
             <p>Author(s): {bookV.authors}</p>
-            <img src={bookImg.thumbnail} />
+            <img src={bookImg === undefined ? "" : `${bookImg.thumbnail}`} />
         </div>
     )
 }
