0b1e2ea0bfeffb29eaf8f362cabd585cfce480f4
Kaitlyn Greve <kgreve14@gmail.com>
2023-03-14 10:58:58 -0700

    styled some

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,13 +1,14 @@
-
+import styles from './intro.module.css';
 
 export default function Intro() {
     return (
-        <div>
-            <p>Introducing Book Look - the ultimate app for all book lovers!
+        <div className={styles.container}>
+            <p className={styles.p}>Introducing Book Look - the ultimate app for all book lovers!
                 With Book Look, you can easily search for any book you desire,
-                read up on detailed information about it, and even save your favorite books to your personalized wishlists and "read list."
+                read up on detailed information about it, and even save your favorite books to your personalized wishlists and "read list".
             </p>
-            <p>To get started with Book Look, you can either search for a book you're interested in, or login to access extra options like adding books to your wishlist or logging books as "read." Happy reading!</p>
+            <p className={styles.p}>To get started with Book Look, you can either search for a book you're interested in, or login to access extra options like adding books to your wishlist or logging books as "read."</p>
+            <p className={styles.p}>Happy reading!</p>
         </div>
     )
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,8 @@
 import { auth } from "../pages/firebase-config";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
+import styles from './login.module.css'
+
 export default function Login() {
 
     const signInWithGoogle = () => {
@@ -9,8 +11,12 @@ export default function Login() {
     };
 
     return (
-        <div>
-            <button onClick={signInWithGoogle}>Login</button>
+        <div className={styles.container}>
+            <button
+                className="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm"
+                onClick={signInWithGoogle}>
+                Login
+            </button>
         </div>
     )
 }
